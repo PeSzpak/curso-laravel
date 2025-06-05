79c285a2f576e9986432b92286ec3b67f693e3e1
Pedro Szpak <dropszpalves@gmail.com>
2025-06-05 11:06:33 -0300

    feat: added route groups for better organization and structure
    - Grouped related routes using route group middleware and prefixes
    - Improved code readability and route management

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ Route::get('/test', function () {
 
 Route:: get ("/hello", [ExampleController::class, "printHelloWorld"]);
 
+Route::prefix("/admin")->group(function(){
+    Route::post("/salvar-produto", [ProductsController::class, "store"])->name("products.store");
+    Route::get("/produtos", [ProductsController::class, "index"])->name("index");
+    Route::get("/produto/{id}", [ProductsController::class, "show"])->name("products.show");
+    Route::get("/editar-produto/{id}", [ProductsController::class, "edit"])->name("products.edit");
+    Route::post("/atualizar-produto/{id}", [ProductsController::class, "update"])->name("products.update");
+    Route::get("/cadastrar-produto", [ProductsController::class, "create"])->name("products.create");
+});
 
-Route::get("/cadastrar-produto", [ProductsController::class, "create"])->name("products.create");
-Route::post("/salvar-produto", [ProductsController::class, "store"])->name("products.store");
-Route::get("/produtos", [ProductsController::class, "index"])->name("index");
-Route::get("/produto/{id}", [ProductsController::class, "show"])->name("products.show");
-Route::get("/editar-produto/{id}", [ProductsController::class, "edit"])->name("products.edit");
-Route::post("/atualizar-produto/{id}", [ProductsController::class, "update"])->name("products.update");
-
-Route::redirect("/produtos", "/cadastrar-produto", 301);
+//Route::redirect("/produtos", "/cadastrar-produto", 301);
 //301 indica um redirect de url 
