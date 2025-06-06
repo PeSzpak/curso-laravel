94e655c06d215d9eb87c4b616b0daa2c995922b7
Pedro Szpak <dropszpalves@gmail.com>
2025-06-06 11:47:11 -0300

    feat: added route access restrictions using auth middleware
    - Protected routes to ensure only authenticated users can access certain pages
    - Applied middleware to controllers and route groups
    - Improved application security and user flow

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,38 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->prefix("/admin")->group(function(){
+    Route::get("/produtos", [ProductsController::class, "index"])->name("products.index");
+    Route::get("/cadastrar-produto", [ProductsController::class, "create"])->name("products.create");
+    Route::post("/salvar-produto", [ProductsController::class, "store"])->name("products.store");
+    Route::get("/produto/{id}", [ProductsController::class, "show"])->name("products.show");
+    Route::get("/editar-produto/{id}", [ProductsController::class, "edit"])->name("products.edit");
+    Route::post("/atualizar-produto/{id}", [ProductsController::class, "update"])->name("products.update");
+    Route::get("/excluir-produto/{id}", [ProductsController::class, "destroy"])->name("products.destroy");
 });
 
-require __DIR__.'/auth.php';
+Route::get('/hello', function () {
+    return view('hello-world');
+});
 
 Auth::routes();
 
